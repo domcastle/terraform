f72dbaa13f61d4fa3e237ac1b6f9ccf4f92b8d37
domcastle <capt1999@gmail.com>
2025-12-10 22:08:27 +0900

    4th commit

--- 02/one-server/main.tf
+++ 02/one-server/main.tf
@@ -5,6 +5,14 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-00e428798e77d38d9"
   instance_type = "t2.micro"
+
+  user_data_replace_on_change = true
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello World" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
+
   tags = {
     Name = "terraform example"
   }
